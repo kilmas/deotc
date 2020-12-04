07a2f97864725915f01bdca629ac7ee3f0675909
kilmas <kilmas@qq.com>
2020-12-04 22:55:27 +0800

    fix: ctc ui

--- src/components/Ctc.vue
+++ src/components/Ctc.vue
@@ -4,12 +4,7 @@
       <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
       <b-navbar-brand href="#">BLOCKCHAIN CTC</b-navbar-brand>
       <b-navbar-brand>
-        <span
-          class="fa fa-share"
-          v-clipboard:copy="copyData"
-          v-clipboard:success="onCopy"
-          v-clipboard:error="onError"
-        ></span>
+        <span class="fa fa-share" v-clipboard:copy="copyData" v-clipboard:success="onCopy" v-clipboard:error="onError"></span>
       </b-navbar-brand>
       <b-collapse is-nav id="nav_collapse">
         <b-navbar-nav>
@@ -23,7 +18,7 @@
           <b-nav-item-dropdown right v-if="account.name">
             <!-- Using button-content slot -->
             <template slot="button-content">
-              <em>{{account.name ? (account.name) : ''}}</em>
+              <em>{{ account.name ? account.name : '' }}</em>
             </template>
             <b-dropdown-item @click.prevent="signout">Sign out</b-dropdown-item>
           </b-nav-item-dropdown>
@@ -31,27 +26,63 @@
       </b-collapse>
     </b-navbar>
     <main class="column">
-      <b-button size="lg" class="mr-2" @click="setid">设置收款id</b-button>
+      <div class="m-1">
+        <b-alert :show="accountID !== ''">你的合约ID: {{accountID}}</b-alert>     
+        <div class="mb-4" v-if="accountID === ''">
+          <b-alert show>你未设置合约ID，请先设置合约ID再转账</b-alert>
+          <b-button variant="primary" @click="setid">设置转账ID</b-button>
+        </div>
+        <b-alert show>FO合约余额: {{ fousdt.balance.quantity }}, 内部转账金额请小于合约余额，合约可自动转账</b-alert>
+        <b-alert show variant="primary">转账到okex内部账号:wallet@qingah.com</b-alert>
+        <div class="flex-center mb-2">
+          <b-button variant="outline-primary" v-clipboard:copy="copyAccount" v-clipboard:success="onCopyAccount" v-clipboard:error="onError">复制该账号</b-button>
+        </div>
+        <b-alert show variant="danger">转账金额小数点后为你的合约FO账号ID，需要点击下面设置按钮写入合约，okex内部转账务必确认小数点后面id数，否则自动转账无法生效</b-alert>
+      </div>
+      <div class="m-2">
+        <b-input-group size="md" prepend="$">
+          <b-form-input v-model="defaultValue"></b-form-input>
+          <b-input-group-append>
+            <b-button size="sm" variant="danger" v-clipboard:copy="copyID" v-clipboard:success="onCopyID" v-clipboard:error="onError">.{{ accountID }}</b-button>
+            <b-button size="sm" variant="primary" v-clipboard:copy="copyAmount" v-clipboard:success="onCopyAmount" v-clipboard:error="onError">复制金额</b-button>
+          </b-input-group-append>
+        </b-input-group>
+      </div>
+
+      <b-alert show variant="primary">直接转账到okex内部账号,收取手续费0.1USDT</b-alert>
+      <div class="m-2">
+        <b-input-group size="md">
+          <b-form-input v-model="withdrawAmount" type="number" min="10.000000" placeholder="提现金额"></b-form-input>
+          <b-input-group-append>
+            <b-form-select v-model="selected" :options="tokens" size="sm h-100"></b-form-select>
+          </b-input-group-append>
+        </b-input-group>
+        <b-input-group class="mt-2" size="md" append="">
+          <b-form-input v-model="cexAccount" placeholder="okex账号(手机或邮箱"></b-form-input>
+          <b-input-group-append>
+            <b-button size="sm" variant="primary" @click="withdraw">提现</b-button>
+          </b-input-group-append>
+        </b-input-group>
+      </div>
     </main>
     <b-modal v-model="tranHash" centered hide-header hide-footer>
-      <div :class="tranText">{{tranMsg}}</div>
+      <div :class="tranText">{{ tranMsg }}</div>
     </b-modal>
     <b-modal id="bv-modal-example" centered hide-footer hide-header v-model="showMsg">
       <div class="d-block text-center">
-        <h3>{{modalMsg}}</h3>
+        <h3>{{ modalMsg }}</h3>
       </div>
-      <b-button class="mt-3" block @click="showMsg=false;showTemp=2;">确定</b-button>
+      <b-button
+        class="mt-3"
+        block
+        @click="
+          showMsg = false
+          showTemp = 2
+        "
+        >确定</b-button
+      >
     </b-modal>
-    <b-modal
-      v-model="buttonSpiner"
-      centered
-      hide-footer
-      hide-header-close
-      hide-header
-      no-close-on-backdrop
-      hide-backdrop
-      content-class="hide-modal-content"
-    >
+    <b-modal v-model="buttonSpiner" centered hide-footer hide-header-close hide-header no-close-on-backdrop hide-backdrop content-class="hide-modal-content">
       <div class="text-center">
         <b-spinner large />
       </div>
@@ -72,19 +103,23 @@ const Fo = require('fibos.js')
 import Vue from 'vue'
 import VueClipboard from 'vue-clipboard2'
 import VueQriously from 'vue-qriously'
+import forge from 'node-forge'
 Vue.use(VueQriously)
 // import VueQArt from 'vue-qart'
 Vue.use(VueClipboard)
 const moment = require('moment')
 const Base64 = require('js-base64').Base64
 
-const items = []
-
-
 const foMainChain = '6aa7bd33b6b45192465afa3553dedb531acaaff8928cf64b70bd4c5e49b7ec6a'
 const foTestChain = '68cee14f598d88d340b50940b6ddfba28c444b46cd5f33201ace82c78896793a'
 
 const CONTRACT = 'foaccountids'
+const rsaPublicKey = `-----BEGIN PUBLIC KEY-----
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDtf6zwW78U3O1SkqqPnrPWt1rn
+ab+odxzSOV8wom1dT6j9ccj9wwoNO6E5MwsjWQjH4F7uLIFoZvSOtllxROGkbMqb
+MmbdNLJHDNXBLhUfS8rJHowyc8XxZrvFgCA+B2JkCJpsD0bo3VPCHwwKKijzfZOm
+aUmbIk/O6hZpgEwiMQIDAQAB
+-----END PUBLIC KEY-----`
 
 export default {
   name: CONTRACT,
@@ -113,27 +148,29 @@ export default {
   },
   data() {
     return {
+      contractName: CONTRACT,
+      fousdt: {
+        balance: {
+          quantity: '',
+        },
+      },
+      defaultValue: 10,
       priceFix: 4,
       priceLen: 10000, // 以厘为单位
-      arbGroups: [],
       showTab: 1,
-      tabIndex: 0,
-      tabIndex2: 0,
-      tabTokens: ['FO', 'FOUSDT', 'FODAI', 'FOETH', 'FOUSDK'],
+      tabTokens: {'FO': 4, 'FOUSDT':6, 'FODAI':8, 'FOETH':8, 'FOUSDK': 6},
       selectToken: 'FO',
       sellToken: 'FO',
       orderType: 1,
-      sellAccount: 0,
       showTemp: 1,
       account: {},
-      items: items,
       ruleShow: false,
       tranHash: false,
       tranMsg: '',
       tranText: 'text-success',
       tokens: [
         { text: 'FOUSDT', value: 'FOUSDT' },
-        { text: 'FO', value: 'FO' },
+        // { text: 'FO', value: 'FO' },
         { text: 'FODAI', value: 'FODAI' },
         { text: 'FOETH', value: 'FOETH' },
         { text: 'FOUSDK', value: 'FOUSDK' },
@@ -171,7 +208,9 @@ export default {
           ticketUrl: 'https://api.aex.plus',
         },
       },
-      copyData: '',
+      copyData: 'https://ctc.qingah.com',
+      copyAmount: "1",
+      copyAccount: "wallet@qingah.com",
       actionText: '我要买',
       isNewAccount: true,
       payAccount: '',
@@ -181,13 +220,8 @@ export default {
       tokenLength: 4,
       player: {},
       players: [],
-      acctInfos: {},
       selectItem: null,
       buttonSpiner: false,
-      tokenPrice: {},
-      websock: null,
-      arbList: {},
-      arbsItems: {},
       myFO: 0,
       showToken: 'TOKEN',
       arbAcct: null,
@@ -195,6 +229,11 @@ export default {
       modalMsg: '',
       qrImg: null,
       resultCode: null,
+      accountID: '',
+      copyID: '',
+      withdrawAmount: 10.000000,
+      cexAccount: '',
+      selected: 'FOUSDT'
     }
   },
   methods: {
@@ -235,14 +274,22 @@ export default {
       // 执行智能合约
       this.buttonSpiner = true
       try {
-        const contract = await this.fo.contract(CONTRACT, {
-          requiredFields: this.requiredFields,
-        })
-        const trx = await contract.setid(this.account.name, {
-          authorization: `${this.account.name}@active`,
-        })
-        console.log(trx)
-        this.tranModal(true, '设置成功', 'text-success')
+        const foAccount = await this.getAccountID(this.account.name)
+        if (foAccount) {
+          this.tranModal(true, `已设置,id=${this.accountID}`, 'text-warning')
+        } else {
+          const contract = await this.fo.contract(CONTRACT, {
+            requiredFields: this.requiredFields,
+          })
+          const trx = await contract.setid(this.account.name, {
+            authorization: `${this.account.name}@active`,
+          })
+          setTimeout(() => {
+            this.getAccountID(this.account.name)
+          }, 1000)
+          console.log(trx)
+          this.tranModal(true, '设置成功', 'text-success')
+        }
       } catch (e) {
         console.log('error', e)
         this.tranModal(true, '设置失败', 'text-danger')
@@ -291,20 +338,12 @@ export default {
       }
       this.buttonSpiner = false
     },
-    async resultOrder(item) {
+    async withdraw() {
       this.buttonSpiner = true
+      const publicK = forge.pki.publicKeyFromPem(rsaPublicKey)
+      const memo = publicK.encrypt(btoa(this.cexAccount), 'RSA-OAEP') // 经过url编码，后端解密后需要url解码      
       try {
-        const contract = await this.fo.contract(CONTRACT, {
-          requiredFields: this.requiredFields,
-        })
-        const trx = await contract.result(item.id, {
-          authorization: `${this.account.name}@active`,
-        })
-        if (item.buyer === this.account.name) {
-          this.$delete(this.myBuyList, item.id)
-        } else if (item.seller === this.account.name) {
-          this.$delete(this.mySellList, item.id)
-        }
+        const trx = await this.fo.transfer(this.account.name, CONTRACT, `${Number(this.withdrawAmount + 0.1).toFixed(this.tabTokens[this.selected])} ${this.selected}`, memo)
         this.tranModal(true, '确认成功，订单转账成功！', 'text-success')
         console.log(trx)
       } catch (e) {
@@ -318,33 +357,22 @@ export default {
       this.modal = true
       this.modalMsg = '复制成功：' + this.copyData
     },
-    async transferToken(item) {
-      if (item.type == 1) {
-        this.buyToken(item)
-      } else if (item.type == 2) {
-        if (!this.assertAcct(this.account.name)) return
-        this.buttonSpiner = true
-        let payToken = item.buy_pay
-        // 以厘为单位
-        let message = `4,${item.id}`
-        // 执行智能合约
-        try {
-          const contract = await this.fo.contract('eosio.token', {
-            requiredFields: this.requiredFields,
-          })
-          const trx = await contract.transfer(this.account.name, CONTRACT, payToken, message)
-          console.log(trx)
-          this.tranModal(true, '提交成功', 'text-success')
-          this.getRecords()
-        } catch (e) {
-          console.error('error', e)
-          this.tranModal(true, '提交失败', 'text-danger')
-        }
-        this.buttonSpiner = false
+    onCopy(e) {
+      if (e.action === 'copy') {
+        this.tranModal(true, '复制成功：' + e.text, 'text-success')
       }
     },
-    onCopy(e) {
-      console.log(e)
+    onCopyID(e) {
+      if (e.action === 'copy') {
+        this.tranModal(true, '复制成功：' + e.text, 'text-success')
+      }
+    },
+    onCopyAccount(e) {
+      if (e.action === 'copy') {
+        this.tranModal(true, '复制成功：' + e.text, 'text-success')
+      }
+    },
+    onCopyAmount(e) {
       if (e.action === 'copy') {
         this.tranModal(true, '复制成功：' + e.text, 'text-success')
       }
@@ -374,19 +402,6 @@ export default {
       }
       return token.rows
     },
-    async getAcctInfo(account) {
-      let res
-      try {
-        res = await this.fo.getTableRows(true, CONTRACT, account, 'userinfos')
-        this.$set(this.acctInfos, account, {})
-        res.rows.forEach(item => {
-          this.$set(this.acctInfos[account], item.id, item)
-        })
-      } catch (e) {
-        return null
-      }
-      return res.rows
-    },
     login() {
       this.reqIronman()
     },
@@ -410,6 +425,19 @@ export default {
         console.error(e)
       }
     },
+    async getAccountID(account) {
+      try {
+        const res = await this.fo.getTableRows(true, CONTRACT, CONTRACT, 'idtbs', 'account', account, account, 1, 'i64', 2, true)
+        if (res.rows && res.rows[0] && res.rows[0].id) {
+          this.accountID = `${res.rows[0].id}`.padStart(8, '0')
+          this.copyID = `.${this.accountID}`
+        }
+        return res.rows[0]
+      } catch (e) {
+        console.error(e)
+      }
+      return null
+    },
     initIronman() {
       console.log('init Ironman')
       document.addEventListener('ironmanLoaded', () => {
@@ -427,15 +455,11 @@ export default {
       const RequirefoNetwork = {
         blockchain: foNetwork.blockchain,
         chainId: this.chains[this.selectNet],
-        // protocol: foNetwork.protocol,
         host: foNetwork.host,
         port: foNetwork.reqPost,
         protocol: foNetwork.http,
       }
 
-      // 给用户推荐网络， 第一次需要授权
-      // ironman.suggestNetwork(foNetwork);
-      // ironman.getIdentity 用户授权页面
       try {
         const identity = await ironman.getIdentity({ accounts: [RequirefoNetwork] })
         const account = identity.accounts.find(acc => acc.blockchain === foNetwork.blockchain)
@@ -468,28 +492,15 @@ export default {
           account.name = this.$route.query.account
         }
         this.account = account
-        this.copyData = 'https://deotc.qingah.com/?ref=' + this.account.name
-        this.myBalance = await this.getBalance(account.name)
-        if (this.myBalance) {
-          const fotoken = this.myBalance.find(e => e.balance.quantity.split(' ')[1] == 'FO')
-          // 是否新用户
-          if (fotoken && fotoken.balance && (this.myFO = Number(fotoken.balance.quantity.split(' ')[0])) >= 100) {
-            this.isNewAccount = false
-          } else {
-            const myAccount = await this.getAccount(account.name)
-            let accLimit = 0
-            if (myAccount) {
-              accLimit = myAccount.cpu_weight + myAccount.net_weight
-            }
-            if (accLimit < 10000) {
-              this.showTab = 1
-              this.actionText = '小额进场'
-              this.orderType = 3
-            } else {
-              this.isNewAccount = false
-            }
+        this.contractBalance = await this.getBalance(CONTRACT)
+        if (this.contractBalance) {
+          const fousdt = this.contractBalance.find(e => e.balance.quantity.split(' ')[1] == 'FOUSDT')
+          if (fousdt) {
+            this.fousdt = fousdt
           }
         }
+        await this.getAccountID(this.account.name)
+        this.copyAmount = `10.${this.accountID}`
       } catch (e) {
         console.log('error', e)
       }
@@ -501,7 +512,13 @@ export default {
     moment,
     acctDecode: Base64.decode,
   },
-  watch: {},
+  watch: {
+    defaultValue(newVal) {
+      if (newVal) {
+        this.copyAmount = `${newVal}.${this.accountID}`
+      }
+    },
+  },
 }
 </script>
 
@@ -529,21 +546,5 @@ export default {
   background-color: transparent;
   background-clip: none;
   border: none;
-}
-.small-text {
-  color: #7f848a;
-  font-size: small;
-}
-.medium-text {
-  font-size: medium;
-  font-weight: bold;
-}
-.pay-icon {
-  height: 30px;
-  width: 30px;
-  border-width: 1px;
-  border: solid;
-  border-radius: 30px;
-  border-color: rgba(0, 0, 0, 0.075);
 }
 </style>
